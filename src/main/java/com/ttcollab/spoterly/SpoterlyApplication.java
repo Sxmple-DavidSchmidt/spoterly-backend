24093f7adc172abdd0aafb9350129e9d57c36c1a
codingtim-dev <tmth.brandt@gmail.com>
2024-09-30 22:57:06 +0200

    init project

---
+++ src/main/java/com/ttcollab/spoterly/SpoterlyApplication.java
@@ -0,0 +1,13 @@
+package com.ttcollab.spoterly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoterlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoterlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttcollab/spoterly/SpoterlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcollab.spoterly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoterlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
